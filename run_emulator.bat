2daab3d810370df5a8219bee9e29e5613b03fe42
phoneticsushi <>
2021-04-11 19:22:02 -0500

    Update batch files to keep the window open in case of error

--- run_emulator.bat
+++ run_emulator.bat
@@ -2,4 +2,5 @@
 
 set LUA_PATH=%CD%\src\bizhawk_bindings.lua
 
-"path/to/Emuhawk.exe" --socket_ip=127.0.0.1 --socket_port=19938 --lua="%LUA_PATH%"
+echo Trying to run Emuhawk...
+"path/to/Emuhawk.exe" --socket_ip=127.0.0.1 --socket_port=19938 --lua="%LUA_PATH%" || pause
