1498f665eff1515e2e48896a693e676336b6f616
phoneticsushi <phoneticsushi@users.noreply.github.com>
2022-09-25 20:43:52 -0400

    Prompt in run_emulator.bat instead of requiring manual edit

--- run_emulator.bat
+++ run_emulator.bat
@@ -8,6 +8,12 @@ if not exist "res\musicReference.ods" (
 )
 
 set LUA_PATH=%CD%\src\bizhawk_bindings.lua
+set EMUHAWK_LINK_NAME="link_to_emuhawk"
+
+if not exist "$EMUHAWK_LINK_NAME" (
+    set /p PATH_TO_EMUHAWK=Enter path to Emuhawk.exe:
+    mklink "$EMUHAWK_LINK_NAME" "$PATH_TO_EMUHAWK"
+)
 
 echo Trying to run Emuhawk...
-"path/to/Emuhawk.exe" --socket_ip=127.0.0.1 --socket_port=19938 --lua="%LUA_PATH%" || pause
+"$EMUHAWK_LINK_NAME" --socket_ip=127.0.0.1 --socket_port=19938 --lua="%LUA_PATH%" || pause
